1a0db954b6edb14011dcd2485066e81c52b8dcb7
Md Shadan Altmash <mdshadanaltmash44@gmail.com>
2021-01-02 10:42:13 +0530

    Flask Migrate

---
+++ FLASK_TUTORIAL/FLASK/Databases/Model_and_CRUD_Basics/README.txt
@@ -0,0 +1,15 @@
+Steps to do Migrate
+1. from flask_migrate import Migrate
+2. After creating your app and db, call Migrate class with your app and db as parameters. e.g Migrate(app,db)
+3. save
+4. in cmd
+  a. set FLASK_APP=filename.py (your main file name)
+  b. flask db init
+  c. db migrate -m "your message"
+  d. flask db upgrade
+
+
+Now if you do any changes then you will have to below Steps
+1. save the changes
+2. db migrate -m "your message"
+3. flask db upgrade
--- FLASK_TUTORIAL/FLASK/Databases/Model_and_CRUD_Basics/basic.py
+++ FLASK_TUTORIAL/FLASK/Databases/Model_and_CRUD_Basics/basic.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -10,7 +11,7 @@ app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///'+os.path.join(basedir,'data.sq
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 
 db=SQLAlchemy(app)
-
+Migrate(app,db)
 class Puppy(db.Model):
     #Manual choice of Database Name
     __tablename__='puppies'
@@ -18,9 +19,11 @@ class Puppy(db.Model):
     id=db.Column(db.Integer,primary_key=True)
     name=db.Column(db.Text)
     age=db.Column(db.Integer)
-    def __init__(self,name,age):
+    breed=db.Column(db.Text)
+    def __init__(self,name,age,breed):
         self.name=name
         self.age=age
+        self.breed=breed
 
     def __repr__(self):
         return f"{self.name} is {self.age} years old"
